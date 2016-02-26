cbb42f5923f0e52174f0d36c4ee23fc96289d627
seabiscuit <hmkhan@qq.com>
2016-02-26 17:21:06 +0800

    upgrade extensions.logging

---
+++ src/CuteAnt.Extensions.Logging.NLog/NLogLogger.cs
@@ -0,0 +1,119 @@
+﻿using System;
+#if DESKTOPCLR
+using MS_ILogger = CuteAnt.Extensions.Logging.ILogger;
+using MS_LogLevel = CuteAnt.Extensions.Logging.LogLevel;
+using MS_LogFormatter = CuteAnt.Extensions.Logging.LogFormatter;
+#else
+using MS_ILogger = Microsoft.Extensions.Logging.ILogger;
+using MS_LogLevel = Microsoft.Extensions.Logging.LogLevel;
+using MS_LogFormatter = Microsoft.Extensions.Logging.LogFormatter;
+#endif
+using NLog_ILogger = NLog.ILogger;
+using NLog_LogLevel = NLog.LogLevel;
+using NLog;
+
+namespace CuteAnt.Extensions.Logging.NLog
+{
+  /// <summary>
+  /// Wrap NLog's Logger in a Microsoft.Extensions.Logging's interface <see cref="Microsoft.Extensions.Logging.ILogger"/>.
+  /// </summary>
+  internal class NLogLogger : MS_ILogger
+  {
+    private readonly NLog_ILogger _logger;
+
+    public NLogLogger(NLog_ILogger logger)
+    {
+      _logger = logger;
+    }
+
+    //todo  callsite showing the framework logging classes/methods
+
+    public void Log(MS_LogLevel logLevel, int eventId, object state, Exception exception, Func<object, Exception, string> formatter)
+    {
+      var nLogLogLevel = ConvertLogLevel(logLevel);
+      if (IsEnabled(nLogLogLevel))
+      {
+        string message;
+        if (formatter != null)
+        {
+          message = formatter(state, exception);
+        }
+        else
+        {
+          message = MS_LogFormatter.Formatter(state, exception);
+        }
+        if (!string.IsNullOrEmpty(message))
+        {
+
+          //message arguments are not needed as it is already checked that the loglevel is enabled.
+          var eventInfo = LogEventInfo.Create(nLogLogLevel, _logger.Name, message);
+          eventInfo.Exception = exception;
+          eventInfo.Properties["EventId"] = eventId;
+          _logger.Log(eventInfo);
+        }
+      }
+    }
+
+    /// <summary>
+    /// Is logging enabled for this logger at this <paramref name="logLevel"/>?
+    /// </summary>
+    /// <param name="logLevel"></param>
+    /// <returns></returns>
+    public bool IsEnabled(MS_LogLevel logLevel)
+    {
+      var convertLogLevel = ConvertLogLevel(logLevel);
+      return IsEnabled(convertLogLevel);
+    }
+
+    /// <summary>
+    /// Is logging enabled for this logger at this <paramref name="logLevel"/>?
+    /// </summary>
+    private bool IsEnabled(NLog_LogLevel logLevel)
+    {
+      return _logger.IsEnabled(logLevel);
+    }
+
+    /// <summary>
+    /// Convert loglevel to NLog variant.
+    /// </summary>
+    /// <param name="logLevel">level to be converted.</param>
+    /// <returns></returns>
+    private static NLog_LogLevel ConvertLogLevel(MS_LogLevel logLevel)
+    {
+      //note in RC2 verbose = trace
+      //https://github.com/aspnet/Logging/pull/314
+      switch (logLevel)
+      {
+
+        case MS_LogLevel.Debug:
+          //note in RC1 trace is verbose is lower then Debug
+          return NLog_LogLevel.Trace;
+        case MS_LogLevel.Verbose:
+          return NLog_LogLevel.Debug;
+        case MS_LogLevel.Information:
+          return NLog_LogLevel.Info;
+        case MS_LogLevel.Warning:
+          return NLog_LogLevel.Warn;
+        case MS_LogLevel.Error:
+          return NLog_LogLevel.Error;
+        case MS_LogLevel.Critical:
+          return NLog_LogLevel.Fatal;
+        case MS_LogLevel.None:
+          return NLog_LogLevel.Off;
+        default:
+          return NLog_LogLevel.Debug;
+      }
+    }
+
+
+    public IDisposable BeginScopeImpl(object state)
+    {
+      if (state == null)
+      {
+        throw new ArgumentNullException(nameof(state));
+      }
+      //TODO not working with async
+      return NestedDiagnosticsContext.Push(state.ToString());
+    }
+  }
+}
--- src/CuteAnt.Extensions.Logging.NLog/NLogLoggerFactoryExtensions.cs
+++ src/CuteAnt.Extensions.Logging.NLog/NLogLoggerFactoryExtensions.cs
@@ -1,21 +1,76 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿using System.IO;
+using System.Reflection;
+using CuteAnt.Extensions.Logging.NLog;
+#if !DESKTOPCLR
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
+#endif
+using NLog_LogManager = NLog.LogManager;
+using NLog.Config;
 
-using System;
-
+#if DESKTOPCLR
 namespace CuteAnt.Extensions.Logging
+#else
+namespace Microsoft.Extensions.Logging
+#endif
 {
+  /// <summary>
+  /// Helpers for ASP.NET
+  /// </summary>
+  public static class NLogLoggerFactoryExtensions
+  {
     /// <summary>
-    /// Summary description for NLogLoggerFactoryExtensions
+    /// Enable NLog as logging provider in ASP.NET 5.
     /// </summary>
-    public static class NLogLoggerFactoryExtensions
+    /// <param name="factory"></param>
+    /// <returns></returns>
+    public static ILoggerFactory AddNLog(this ILoggerFactory factory)
     {
-        public static ILoggerFactory AddNLog(
-            this ILoggerFactory factory,
-            global::NLog.LogFactory logFactory)
-        {
-            factory.AddProvider(new NLog.NLogLoggerProvider(logFactory));
-            return factory;
-        }
+      //ignore this
+#if NET40
+      NLog_LogManager.AddHiddenAssembly(typeof(NLogLoggerFactoryExtensions).Assembly);
+#else
+      NLog_LogManager.AddHiddenAssembly(typeof(NLogLoggerFactoryExtensions).GetTypeInfo().Assembly);
+#endif
+
+      using (var provider = new NLogLoggerProvider())
+      {
+        factory.AddProvider(provider);
+      }
+      return factory;
     }
+
+#if !DESKTOPCLR
+    /// <summary>
+    /// Apply NLog configuration from XML config.
+    /// </summary>
+    /// <param name="env"></param>
+    /// <param name="configFileRelativePath">relative path to NLog configuration file.</param>
+    public static void ConfigureNLog(this IHostingEnvironment env, string configFileRelativePath)
+    {
+      var fileName = Path.Combine(Directory.GetParent(env.WebRootPath).FullName, configFileRelativePath);
+      ConfigureNLog(fileName);
+    }
+
+    /// <summary>
+    /// Apply NLog configuration from XML config.
+    /// </summary>
+    /// <param name="env"></param>
+    /// <param name="configFileRelativePath">relative path to NLog configuration file.</param>
+    public static void ConfigureNLog(this IApplicationEnvironment env, string configFileRelativePath)
+    {
+      var fileName = Path.Combine(env.ApplicationBasePath, configFileRelativePath);
+      ConfigureNLog(fileName);
+    }
+#endif
+
+    /// <summary>
+    /// Apply NLog configuration from XML config.
+    /// </summary>
+    /// <param name="fileName">absolute path  NLog configuration file.</param>
+    private static void ConfigureNLog(string fileName)
+    {
+      NLog_LogManager.Configuration = new XmlLoggingConfiguration(fileName, true);
+    }
+  }
 }
