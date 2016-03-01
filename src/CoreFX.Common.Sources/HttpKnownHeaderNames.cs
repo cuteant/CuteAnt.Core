7b614571e44d743a264ee1dd19c4e6f09ba800b9
seabiscuit <hmkhan@qq.com>
2016-03-01 16:55:39 +0800

    CoreFX.Common.Sources 目录整理

---
+++ src/CoreFX.Common.Sources/HttpKnownHeaderNames.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Net
+{
+  /// <summary>Summary description for HttpKnownHeaderNames</summary>
+  internal static class HttpKnownHeaderNames
+  {
+    internal const String CacheControl = "Cache-Control";
+    internal const String Connection = "Connection";
+    internal const String Date = "Date";
+    internal const String KeepAlive = "Keep-Alive";
+    internal const String Pragma = "Pragma";
+    internal const String ProxyConnection = "Proxy-Connection";
+    internal const String Trailer = "Trailer";
+    internal const String TransferEncoding = "Transfer-Encoding";
+    internal const String Upgrade = "Upgrade";
+    internal const String Via = "Via";
+    internal const String Warning = "Warning";
+    internal const String ContentLength = "Content-Length";
+    internal const String ContentType = "Content-Type";
+    internal const String ContentDisposition = "Content-Disposition";
+    internal const String ContentEncoding = "Content-Encoding";
+    internal const String ContentLanguage = "Content-Language";
+    internal const String ContentLocation = "Content-Location";
+    internal const String ContentRange = "Content-Range";
+    internal const String Expires = "Expires";
+    internal const String LastModified = "Last-Modified";
+    internal const String Age = "Age";
+    internal const String Location = "Location";
+    internal const String ProxyAuthenticate = "Proxy-Authenticate";
+    internal const String RetryAfter = "Retry-After";
+    internal const String Server = "Server";
+    internal const String SetCookie = "Set-Cookie";
+    internal const String SetCookie2 = "Set-Cookie2";
+    internal const String Vary = "Vary";
+    internal const String WWWAuthenticate = "WWW-Authenticate";
+    internal const String Accept = "Accept";
+    internal const String AcceptCharset = "Accept-Charset";
+    internal const String AcceptEncoding = "Accept-Encoding";
+    internal const String AcceptLanguage = "Accept-Language";
+    internal const String Authorization = "Authorization";
+    internal const String Cookie = "Cookie";
+    internal const String Cookie2 = "Cookie2";
+    internal const String Expect = "Expect";
+    internal const String From = "From";
+    internal const String Host = "Host";
+    internal const String IfMatch = "If-Match";
+    internal const String IfModifiedSince = "If-Modified-Since";
+    internal const String IfNoneMatch = "If-None-Match";
+    internal const String IfRange = "If-Range";
+    internal const String IfUnmodifiedSince = "If-Unmodified-Since";
+    internal const String MaxForwards = "Max-Forwards";
+    internal const String ProxyAuthorization = "Proxy-Authorization";
+    internal const String Referer = "Referer";
+    internal const String Range = "Range";
+    internal const String UserAgent = "User-Agent";
+    internal const String ContentMD5 = "Content-MD5";
+    internal const String ETag = "ETag";
+    internal const String TE = "TE";
+    internal const String Allow = "Allow";
+    internal const String AcceptRanges = "Accept-Ranges";
+    internal const String P3P = "P3P";
+    internal const String XPoweredBy = "X-Powered-By";
+    internal const String XAspNetVersion = "X-AspNet-Version";
+    internal const String SecWebSocketKey = "Sec-WebSocket-Key";
+    internal const String SecWebSocketExtensions = "Sec-WebSocket-Extensions";
+    internal const String SecWebSocketAccept = "Sec-WebSocket-Accept";
+    internal const String Origin = "Origin";
+    internal const String SecWebSocketProtocol = "Sec-WebSocket-Protocol";
+    internal const String SecWebSocketVersion = "Sec-WebSocket-Version";
+    internal const string ConnectionUpgrade = "Upgrade";
+
+    internal const String Translate = "Translate";
+  }
+}
