5dce8fd45c6b74051fd6ff80c5495296c8da977a
seabiscuit <cuteant@outlook.com>
2016-04-06 03:14:33 +0800

    CuteAnt.Extensions.Caching.SqlServer

---
+++ src/CuteAnt.Extensions.Caching.SqlServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if NET40
+[assembly: AssemblyTitle("CuteAnt.Extensions.Caching.SqlServer for .NetFx4.0")]
+#elif NET451 || DNX451
+[assembly: AssemblyTitle("CuteAnt.Extensions.Caching.SqlServer for .NetFx4.5")]
+#endif
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("CuteAnt.Extensions.Caching.SqlServer Library (Flavor=Debug)")]
+#else
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyDescription("CuteAnt.Extensions.Caching.SqlServer Library (Flavor=Retail)")]
+#endif
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1790a700-58ba-40f0-9894-20cd6e5608bc")]
---
+++ src/CuteAnt.Extensions.Caching.SqlServer/SqlServerCacheServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using CuteAnt.Extensions.Caching.Distributed;
+using CuteAnt.Extensions.Caching.SqlServer;
+using CuteAnt.Extensions.DependencyInjection.Extensions;
+
+namespace CuteAnt.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods for setting up Microsoft SQL Server distributed cache services in an <see cref="IServiceCollection" />.
+    /// </summary>
+    public static class SqlServerCachingServicesExtensions
+    {
+        /// <summary>
+        /// Adds Microsoft SQL Server distributed caching services to the specified <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <param name="setupAction">An <see cref="Action{SqlServerCacheOptions}"/> to configure the provided <see cref="SqlServerCacheOptions"/>.</param>
+        public static void AddDistributedSqlServerCache(this IServiceCollection services, Action<SqlServerCacheOptions> setupAction)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (setupAction == null)
+            {
+                throw new ArgumentNullException(nameof(setupAction));
+            }
+
+            services.AddOptions();
+            AddSqlServerCacheServices(services);
+            services.Configure(setupAction);
+        }
+
+        // to enable unit testing
+        internal static void AddSqlServerCacheServices(IServiceCollection services)
+        {
+            services.Add(ServiceDescriptor.Singleton<IDistributedCache, SqlServerCache>());
+        }
+    }
+}
