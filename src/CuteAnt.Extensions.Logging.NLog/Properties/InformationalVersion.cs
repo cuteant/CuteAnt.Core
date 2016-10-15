fdd9b8d6f0f363ea440b937762d5434f0fdbf716
seabiscuit <cuteant@outlook.com>
2016-10-16 00:52:45 +0800

    upgrade packages

--- src/CuteAnt.Extensions.Logging.NLog/Properties/InformationalVersion.cs
+++ src/CuteAnt.Extensions.Logging.NLog/Properties/InformationalVersion.cs
@@ -2,5 +2,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyInformationalVersion("2.0.0-rc3-160701")]
+[assembly: AssemblyInformationalVersion("2.0.0-rc3-160801")]
 
--- src/CuteAnt.Extensions.Logging.Serilog/Extensions/Logging/SerilogLoggerScope.cs
+++ src/CuteAnt.Extensions.Logging.Serilog/Extensions/Logging/SerilogLoggerScope.cs
@@ -9,59 +9,68 @@ using Serilog.Events;
 
 namespace Serilog.Extensions.Logging
 {
-    class SerilogLoggerScope : IDisposable, ILogEventEnricher
+  class SerilogLoggerScope : IDisposable, ILogEventEnricher
+  {
+    readonly SerilogLoggerProvider _provider;
+    readonly object _state;
+    readonly IDisposable _popSerilogContext;
+
+    // An optimization only, no problem if there are data races on this.
+    bool _disposed;
+
+    public SerilogLoggerScope(SerilogLoggerProvider provider, object state)
     {
-        readonly SerilogLoggerProvider _provider;
-        readonly object _state;
-        readonly IDisposable _popSerilogContext;
+      _provider = provider;
+      _state = state;
 
-        // An optimization only, no problem if there are data races on this.
-        bool _disposed;
-
-        public SerilogLoggerScope(SerilogLoggerProvider provider, object state)
-        {
-            _provider = provider;
-            _state = state;
-
-            Parent = _provider.CurrentScope;
-            _provider.CurrentScope = this;
-            _popSerilogContext = LogContext.PushProperties(this);
-        }
-
-        public SerilogLoggerScope Parent { get; }
-        
-        public void Dispose()
-        {
-            if (!_disposed)
-            {
-                _disposed = true;
-
-                // In case one of the parent scopes has been disposed out-of-order, don't
-                // just blindly reinstate our own parent.
-                for (var scan = _provider.CurrentScope; scan != null; scan = scan.Parent)
-                {
-                    if (ReferenceEquals(scan, this))
-                        _provider.CurrentScope = Parent;
-                }
-
-                _popSerilogContext.Dispose();
-            }
-        }
-
-        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
-        {
-            var stateProperties = _state as IEnumerable<KeyValuePair<string, object>>;
-            if (stateProperties != null)
-            {
-                foreach (var stateProperty in stateProperties)
-                {
-                    if (stateProperty.Key == SerilogLoggerProvider.OriginalFormatPropertyName && stateProperty.Value is string)
-                        continue;
-
-                    var property = propertyFactory.CreateProperty(stateProperty.Key, stateProperty.Value);
-                    logEvent.AddPropertyIfAbsent(property);
-                }
-            }
-        }
+      Parent = _provider.CurrentScope;
+      _provider.CurrentScope = this;
+      _popSerilogContext = LogContext.PushProperties(this);
     }
+
+    public SerilogLoggerScope Parent { get; }
+
+    public void Dispose()
+    {
+      if (!_disposed)
+      {
+        _disposed = true;
+
+        // In case one of the parent scopes has been disposed out-of-order, don't
+        // just blindly reinstate our own parent.
+        for (var scan = _provider.CurrentScope; scan != null; scan = scan.Parent)
+        {
+          if (ReferenceEquals(scan, this))
+            _provider.CurrentScope = Parent;
+        }
+
+        _popSerilogContext.Dispose();
+      }
+    }
+
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+      var stateProperties = _state as IEnumerable<KeyValuePair<string, object>>;
+      if (stateProperties != null)
+      {
+        foreach (var stateProperty in stateProperties)
+        {
+          if (stateProperty.Key == SerilogLoggerProvider.OriginalFormatPropertyName && stateProperty.Value is string)
+            continue;
+
+          var key = stateProperty.Key;
+          var destructureObject = false;
+
+          if (key.StartsWith("@", StringComparison.Ordinal))
+          {
+            key = key.Substring(1);
+            destructureObject = true;
+          }
+
+          var property = propertyFactory.CreateProperty(key, stateProperty.Value, destructureObject);
+          logEvent.AddPropertyIfAbsent(property);
+        }
+      }
+    }
+  }
 }
