1124cbe00620863f5a17e7f561716951d134d4bc
Seabiscuit <hmkhan@qq.com>
2017-11-23 03:04:48 +0800

    移除无用依赖项

--- src/CuteAnt/Reflection/TypeHelper.cs
+++ src/CuteAnt/Reflection/TypeHelper.cs
@@ -6,9 +6,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.Serialization;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace CuteAnt.Reflection
 {
