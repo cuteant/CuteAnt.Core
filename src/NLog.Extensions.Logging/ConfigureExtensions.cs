7c743d0115947a9a662c8cd643326911c082d40f
Seabiscuit <cuteant@outlook.com>
2017-11-23 17:25:25 +0800

    NLog.Extensions.Logging

---
+++ src/NLog.Extensions.Logging/ConfigureExtensions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+using NLog.Common;
+using NLog.Config;
+
+namespace NLog.Extensions.Logging
+{
+  /// <summary>Helpers for .NET Core</summary>
+  public static class ConfigureExtensions
+  {
+    /// <summary>Enable NLog as logging provider in .NET Core.</summary>
+    /// <param name="factory"></param>
+    /// <returns>ILoggerFactory for chaining</returns>
+    public static ILoggerFactory AddNLog(this ILoggerFactory factory)
+    {
+      return AddNLog(factory, null);
+    }
+
+    /// <summary>Enable NLog as logging provider in .NET Core.</summary>
+    /// <param name="factory"></param>
+    /// <param name="options">NLog options</param>
+    /// <returns>ILoggerFactory for chaining</returns>
+    public static ILoggerFactory AddNLog(this ILoggerFactory factory, NLogProviderOptions options)
+    {
+      ConfigureHiddenAssemblies();
+
+      using (var provider = new NLogLoggerProvider(options))
+      {
+        factory.AddProvider(provider);
+      }
+      return factory;
+    }
+
+    /// <summary>Enable NLog as logging provider in .NET Core.</summary>
+    /// <param name="factory"></param>
+    /// <returns>ILoggerFactory for chaining</returns>
+    public static ILoggingBuilder AddNLog(this ILoggingBuilder factory)
+    {
+      return AddNLog(factory, null);
+    }
+
+    /// <summary>Enable NLog as logging provider in .NET Core.</summary>
+    /// <param name="factory"></param>
+    /// <param name="options">NLog options</param>
+    /// <returns>ILoggerFactory for chaining</returns>
+    public static ILoggingBuilder AddNLog(this ILoggingBuilder factory, NLogProviderOptions options)
+    {
+      ConfigureHiddenAssemblies();
+
+      using (var provider = new NLogLoggerProvider(options))
+      {
+        factory.AddProvider(provider);
+      }
+      return factory;
+    }
+
+    private static void ConfigureHiddenAssemblies()
+    {
+      try
+      {
+        //ignore these assemblies for ${callsite}
+#if NET40
+        LogManager.AddHiddenAssembly(typeof(Microsoft.Extensions.Logging.LoggerFactory).Assembly); //Microsoft.Extensions.Logging
+        LogManager.AddHiddenAssembly(typeof(Microsoft.Extensions.Logging.ILogger).Assembly); // Microsoft.Extensions.Logging.Abstractions
+        LogManager.AddHiddenAssembly(typeof(NLog.Extensions.Logging.ConfigureExtensions).Assembly); //NLog.Extensions.Logging
+#else
+        LogManager.AddHiddenAssembly(typeof(Microsoft.Extensions.Logging.LoggerFactory).GetTypeInfo().Assembly); //Microsoft.Extensions.Logging
+        LogManager.AddHiddenAssembly(typeof(Microsoft.Extensions.Logging.ILogger).GetTypeInfo().Assembly); // Microsoft.Extensions.Logging.Abstractions
+        LogManager.AddHiddenAssembly(typeof(NLog.Extensions.Logging.ConfigureExtensions).GetTypeInfo().Assembly); //NLog.Extensions.Logging
+#endif
+
+        //try
+        //{
+        //  //try the Filter ext
+        //  var filterAssembly = Assembly.Load(new AssemblyName("Microsoft.Extensions.Logging.Filter"));
+        //  LogManager.AddHiddenAssembly(filterAssembly);
+        //}
+        //catch (Exception ex)
+        //{
+        //  InternalLogger.Trace(ex, "filtering Microsoft.Extensions.Logging.Filter failed. Not an issue probably");
+        //}
+      }
+      catch (Exception ex)
+      {
+        InternalLogger.Debug(ex, "failure in ignoring assemblies. This could influence the ${callsite}");
+      }
+    }
+
+    /// <summary>Apply NLog configuration from XML config.</summary>
+    /// <param name="loggerFactory"></param>
+    /// <param name="configFileRelativePath">relative path to NLog configuration file.</param>
+    /// <returns>Current configuration for chaining.</returns>
+    public static LoggingConfiguration ConfigureNLog(this ILoggerFactory loggerFactory, string configFileRelativePath)
+    {
+      var rootPath = AppDomain.CurrentDomain.BaseDirectory;
+
+      var fileName = Path.Combine(rootPath, configFileRelativePath);
+      return ConfigureNLog(fileName);
+    }
+
+    /// <summary>Apply NLog configuration from config object.</summary>
+    /// <param name="loggerFactory"></param>
+    /// <param name="config">New NLog config.</param>
+    /// <returns>Current configuration for chaining.</returns>
+    public static LoggingConfiguration ConfigureNLog(this ILoggerFactory loggerFactory, LoggingConfiguration config)
+    {
+      LogManager.Configuration = config;
+
+      return config;
+    }
+
+    /// <summary>Apply NLog configuration from XML config.</summary>
+    /// <param name="fileName">absolute path NLog configuration file.</param>
+    /// <returns>Current configuration for chaining.</returns>
+    private static LoggingConfiguration ConfigureNLog(string fileName)
+    {
+      var config = new XmlLoggingConfiguration(fileName, true);
+      LogManager.Configuration = config;
+      return config;
+    }
+  }
+}
---
+++ src/NLog.Extensions.Logging/NLogLogger.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace NLog.Extensions.Logging
+{
+  /// <summary>Wrap NLog's Logger in a Microsoft.Extensions.Logging's interface <see cref="Microsoft.Extensions.Logging.ILogger"/>.</summary>
+  internal class NLogLogger : Microsoft.Extensions.Logging.ILogger
+  {
+    private readonly Logger _logger;
+    private readonly NLogProviderOptions _options;
+
+    internal const string OriginalFormatPropertyName = "{OriginalFormat}";
+    private static readonly object EmptyEventId = default(EventId);    // Cache boxing of empty EventId-struct
+    private static readonly object ZeroEventId = default(EventId).Id;  // Cache boxing of zero EventId-Value
+    private Tuple<string, string, string> _eventIdPropertyNames;
+
+    public NLogLogger(Logger logger, NLogProviderOptions options)
+    {
+      _logger = logger;
+      _options = options ?? NLogProviderOptions.Default;
+    }
+
+    public void Log<TState>(Microsoft.Extensions.Logging.LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+      var nLogLogLevel = ConvertLogLevel(logLevel);
+      if (!IsEnabled(nLogLogLevel))
+      {
+        return;
+      }
+      if (formatter == null)
+      {
+        throw new ArgumentNullException(nameof(formatter));
+      }
+      var message = formatter(state, exception);
+
+      var messageTemplate = _options.CaptureMessageTemplates ? state as IReadOnlyList<KeyValuePair<string, object>> : null;
+      LogEventInfo eventInfo = CreateLogEventInfo(nLogLogLevel, message, messageTemplate);
+      eventInfo.Exception = exception;
+
+      CaptureEventId(eventId, eventInfo);
+
+      if (_options.CaptureMessageProperties && messageTemplate == null)
+      {
+        CaptureMessageProperties(state, eventInfo);
+      }
+
+      _logger.Log(eventInfo);
+    }
+
+    private LogEventInfo CreateLogEventInfo(LogLevel nLogLogLevel, string message, IReadOnlyList<KeyValuePair<string, object>> parameterList)
+    {
+      if (parameterList != null && parameterList.Count > 1)
+      {
+        // More than a single parameter (last parameter is the {OriginalFormat})
+        var firstParameterName = parameterList[0].Key;
+        if (!string.IsNullOrEmpty(firstParameterName))
+        {
+          if (firstParameterName.Length != 1 || !char.IsDigit(firstParameterName[0]))
+          {
+#if NETSTANDARD2_0
+            var originalFormat = parameterList[parameterList.Count - 1];
+            string originalMessage = null;
+            if (originalFormat.Key == OriginalFormatPropertyName)
+            {
+              // Attempt to capture original message with placeholders
+              originalMessage = originalFormat.Value as string;
+            }
+
+            var messageTemplateParameters = new NLogMessageParameterList(parameterList, originalMessage != null);
+            var eventInfo = new LogEventInfo(nLogLogLevel, _logger.Name, originalMessage ?? message, messageTemplateParameters);
+            if (originalMessage != null)
+            {
+              eventInfo.Parameters = new object[messageTemplateParameters.Count + 1];
+              for (int i = 0; i < messageTemplateParameters.Count; ++i)
+                eventInfo.Parameters[i] = messageTemplateParameters[i].Value;
+              eventInfo.Parameters[messageTemplateParameters.Count] = message;
+              eventInfo.MessageFormatter = (l) => (string)l.Parameters[l.Parameters.Length - 1];
+            }
+            return eventInfo;
+#else
+            var eventInfo = LogEventInfo.Create(nLogLogLevel, _logger.Name, message);
+            for (int i = 0; i < parameterList.Count; ++i)
+            {
+              var parameter = parameterList[i];
+              if (string.IsNullOrEmpty(parameter.Key))
+                break;  // Skip capture of invalid parameters
+
+              var parameterName = parameter.Key;
+              switch (parameterName[0])
+              {
+                case '@': parameterName = parameterName.Substring(1); break;
+                case '$': parameterName = parameterName.Substring(1); break;
+              }
+              eventInfo.Properties[parameterName] = parameter.Value;
+            }
+            return eventInfo;
+#endif
+          }
+        }
+      }
+      return LogEventInfo.Create(nLogLogLevel, _logger.Name, message);
+    }
+
+    private void CaptureEventId(EventId eventId, LogEventInfo eventInfo)
+    {
+      if (!_options.IgnoreEmptyEventId || eventId.Id != 0 || !string.IsNullOrEmpty(eventId.Name))
+      {
+        // Attempt to reuse the same string-allocations based on the current
+        // <see cref="NLogProviderOptions.EventIdSeparator"/>
+        var eventIdPropertyNames = _eventIdPropertyNames ?? new Tuple<string, string, string>(null, null, null);
+        var eventIdSeparator = _options.EventIdSeparator ?? string.Empty;
+        if (!ReferenceEquals(eventIdPropertyNames.Item1, eventIdSeparator))
+        {
+          // Perform atomic cache update of the string-allocations matching the current separator
+          eventIdPropertyNames = new Tuple<string, string, string>(
+              eventIdSeparator,
+              string.Concat("EventId", eventIdSeparator, "Id"),
+              string.Concat("EventId", eventIdSeparator, "Name"));
+          _eventIdPropertyNames = eventIdPropertyNames;
+        }
+
+        var idIsZero = eventId.Id == 0;
+        eventInfo.Properties[eventIdPropertyNames.Item2] = idIsZero ? ZeroEventId : eventId.Id;
+        eventInfo.Properties[eventIdPropertyNames.Item3] = eventId.Name;
+        eventInfo.Properties["EventId"] = idIsZero && eventId.Name == null ? EmptyEventId : eventId;
+      }
+    }
+
+    private static void CaptureMessageProperties<TState>(TState state, LogEventInfo eventInfo)
+    {
+      if (state is IEnumerable<KeyValuePair<string, object>> messageProperties)
+      {
+        foreach (var property in messageProperties)
+        {
+          if (string.IsNullOrEmpty(property.Key))
+            continue;
+
+          eventInfo.Properties[property.Key] = property.Value;
+        }
+      }
+    }
+
+    /// <summary>Is logging enabled for this logger at this <paramref name="logLevel"/>?</summary>
+    /// <param name="logLevel"></param>
+    /// <returns></returns>
+    public bool IsEnabled(Microsoft.Extensions.Logging.LogLevel logLevel)
+    {
+      switch (logLevel)
+      {
+        case Microsoft.Extensions.Logging.LogLevel.Trace:
+          return _logger.IsTraceEnabled;
+        case Microsoft.Extensions.Logging.LogLevel.Debug:
+          return _logger.IsDebugEnabled;
+        case Microsoft.Extensions.Logging.LogLevel.Information:
+          return _logger.IsInfoEnabled;
+        case Microsoft.Extensions.Logging.LogLevel.Warning:
+          return _logger.IsWarnEnabled;
+        case Microsoft.Extensions.Logging.LogLevel.Error:
+          return _logger.IsErrorEnabled;
+        case Microsoft.Extensions.Logging.LogLevel.Critical:
+          return _logger.IsFatalEnabled;
+        case Microsoft.Extensions.Logging.LogLevel.None:
+        default:
+          return _logger.IsDebugEnabled;
+      }
+      //var convertLogLevel = ConvertLogLevel(logLevel);
+      //return IsEnabled(convertLogLevel);
+    }
+
+    /// <summary>Is logging enabled for this logger at this <paramref name="logLevel"/>?</summary>
+    private bool IsEnabled(LogLevel logLevel)
+    {
+      return _logger.IsEnabled(logLevel);
+    }
+
+    /// <summary>Convert loglevel to NLog variant.</summary>
+    /// <param name="logLevel">level to be converted.</param>
+    /// <returns></returns>
+    private static LogLevel ConvertLogLevel(Microsoft.Extensions.Logging.LogLevel logLevel)
+    {
+      switch (logLevel)
+      {
+        case Microsoft.Extensions.Logging.LogLevel.Trace:
+          return LogLevel.Trace;
+
+        case Microsoft.Extensions.Logging.LogLevel.Debug:
+          return LogLevel.Debug;
+
+        case Microsoft.Extensions.Logging.LogLevel.Information:
+          return LogLevel.Info;
+
+        case Microsoft.Extensions.Logging.LogLevel.Warning:
+          return LogLevel.Warn;
+
+        case Microsoft.Extensions.Logging.LogLevel.Error:
+          return LogLevel.Error;
+
+        case Microsoft.Extensions.Logging.LogLevel.Critical:
+          return LogLevel.Fatal;
+
+        case Microsoft.Extensions.Logging.LogLevel.None:
+          return LogLevel.Off;
+
+        default:
+          return LogLevel.Debug;
+      }
+    }
+
+    private class ScopeProperties : IDisposable
+    {
+      private List<IDisposable> _properties;
+      private List<IDisposable> Properties
+      { get { return _properties ?? (_properties = new List<IDisposable>()); } }
+
+      private class ScopeProperty : IDisposable
+      {
+        private string _key;
+
+        public ScopeProperty(string key, object value)
+        {
+          _key = key;
+          MappedDiagnosticsLogicalContext.Set(key, value);
+        }
+
+        public void Dispose()
+        {
+          MappedDiagnosticsLogicalContext.Remove(_key);
+        }
+      }
+
+      public void AddDispose(IDisposable disposable)
+      {
+        Properties.Add(disposable);
+      }
+
+      public void AddProperty(string key, object value)
+      {
+        AddDispose(new ScopeProperty(key, value));
+      }
+
+      public void Dispose()
+      {
+        var properties = _properties;
+        if (properties != null)
+        {
+          _properties = null;
+          foreach (var property in properties)
+          {
+            try
+            {
+              property.Dispose();
+            }
+            catch
+            {
+            }
+          }
+        }
+      }
+    }
+
+    /// <summary>Begin a scope. Use in config with ${ndlc}</summary>
+    /// <param name="state">The state (message)</param>
+    /// <returns></returns>
+    public IDisposable BeginScope<TState>(TState state)
+    {
+      if (state == null)
+      {
+        throw new ArgumentNullException(nameof(state));
+      }
+
+      if (_options.CaptureMessageProperties)
+      {
+        if (state is IEnumerable<KeyValuePair<string, object>> messageProperties)
+        {
+          ScopeProperties scope = new ScopeProperties();
+
+          foreach (var property in messageProperties)
+          {
+            if (string.IsNullOrEmpty(property.Key))
+              continue;
+
+            scope.AddProperty(property.Key, property.Value);
+          }
+
+          scope.AddDispose(NestedDiagnosticsLogicalContext.Push(state));
+          return scope;
+        }
+      }
+
+      return NestedDiagnosticsLogicalContext.Push(state);
+    }
+  }
+}
