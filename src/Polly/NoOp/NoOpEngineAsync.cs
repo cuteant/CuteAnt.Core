99949139feb38a19e837086a36f53ccf07a7f7ef
Seabiscuit <cuteant@outlook.com>
2017-12-06 16:32:20 +0800

    签入 Polly

---
+++ src/Polly/NoOp/NoOpEngineAsync.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Polly.NoOp
+{
+    internal static partial class NoOpEngine
+    {
+        internal static async Task<TResult> ImplementationAsync<TResult>(Func<Context, CancellationToken, Task<TResult>> action, Context context, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            return await action(context, cancellationToken).ConfigureAwait(continueOnCapturedContext);
+        }
+    }
+}
