7bd7aa8f5acd51b4f2915db68fbde48f892cc6ae
Seabiscuit <hmkhan@qq.com>
2018-04-21 02:27:39 +0800

    Update Polly to v5.9.0

--- src/Polly/Context.cs
+++ src/Polly/Context.cs
@@ -1,59 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
 using Polly.Wrap;
 
 namespace Polly
 {
     /// <summary>
     /// Context that carries with a single execution through a Policy.   Commonly-used properties are directly on the class.  Backed by a dictionary of string key / object value pairs, to which user-defined values may be added.
-    /// <remarks>Do not re-use an instance of <see cref="Context"/> across more than one execution.</remarks>
+    /// <remarks>Do not re-use an instance of <see cref="Context"/> across more than one call through .Execute(...) or .ExecuteAsync(...).</remarks>
     /// </summary>
     public partial class Context
     {
         internal static readonly Context None = new Context();
 
-        private Guid? _executionGuid;
+        private Guid? _correlationId;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Context"/> class, with the specified <paramref name="executionKey"/>.
+        /// Initializes a new instance of the <see cref="Context"/> class, with the specified <paramref name="operationKey"/>.
         /// </summary>
-        /// <param name="executionKey">The execution key.</param>
-        public Context(String executionKey)
+        /// <param name="operationKey">The operation key.</param>
+        public Context(String operationKey)
         {
-            ExecutionKey = executionKey;
+            OperationKey = operationKey;
         }
 
-        internal Context()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Context"/> class.
+        /// </summary>
+        public Context()
         {
         }
 
         /// <summary>
-        /// When execution is through a <see cref="PolicyWrap"/>, identifies the PolicyWrap executing the current delegate by returning the <see cref="Policy.PolicyKey"/> of the outermost layer in the PolicyWrap; otherwise, null.
+        /// When execution is through a <see cref="PolicyWrap"/>, identifies the PolicyWrap executing the current delegate by returning the <see cref="PolicyBase.PolicyKey"/> of the outermost layer in the PolicyWrap; otherwise, null.
         /// </summary>
         public String PolicyWrapKey { get; internal set; }
 
         /// <summary>
-        /// The <see cref="Policy.PolicyKey"/> of the <see cref="Policy"/> instance executing the current delegate.
+        /// The <see cref="PolicyBase.PolicyKey"/> of the <see cref="Policy"/> instance executing the current delegate.
         /// </summary>
         public String PolicyKey { get; internal set; }
 
         /// <summary>
         /// A key unique to the call site of the current execution. 
-        /// <remarks><see cref="Policy"/> instances are commonly reused across multiple call sites.  Set an ExecutionKey so that logging and metrics can distinguish usages of policy instances at different call sites.</remarks>
-        /// <remarks>The value is set by using the <see cref="Context(String)"/> constructor taking an executionKey parameter.</remarks>
+        /// <remarks>The value is set by using the <see cref="Context(String)"/> constructor taking an operationKey parameter.</remarks>
         /// </summary>
-        public String ExecutionKey { get; }
+        [Obsolete("This property is being renamed OperationKey, from Polly v6.")]
+        public String ExecutionKey => OperationKey;
+
+        /// <summary>
+        /// A key unique to the call site of the current execution. 
+        /// <remarks><see cref="Policy"/> instances are commonly reused across multiple call sites.  Set an OperationKey so that logging and metrics can distinguish usages of policy instances at different call sites.</remarks>
+        /// <remarks>The value is set by using the <see cref="Context(String)"/> constructor taking an operationKey parameter.</remarks>
+        /// </summary>
+        public String OperationKey { get; }
 
         /// <summary>
         /// A Guid guaranteed to be unique to each execution.
         /// <remarks>Acts as a correlation id so that events specific to a single execution can be identified in logging and telemetry.</remarks>
         /// </summary>
-        public Guid ExecutionGuid
+        [Obsolete("This property is being renamed CorrelationId, from Polly v6.")]
+        public Guid ExecutionGuid => CorrelationId;
+
+        /// <summary>
+        /// A Guid guaranteed to be unique to each execution.
+        /// <remarks>Acts as a correlation id so that events specific to a single execution can be identified in logging and telemetry.</remarks>
+        /// </summary>
+        public Guid CorrelationId
         {
             get
             {
-                if (!_executionGuid.HasValue) { _executionGuid = Guid.NewGuid(); }
-                return _executionGuid.Value;
+                if (!_correlationId.HasValue) { _correlationId = Guid.NewGuid(); }
+                return _correlationId.Value;
             }
         }
         
