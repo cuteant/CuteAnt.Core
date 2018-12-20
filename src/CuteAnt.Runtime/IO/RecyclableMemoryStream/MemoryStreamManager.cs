8884fedcea7265d6eadcb6313af406478cea32d4
cuteant <cuteant@outlook.com>
2018-12-20 21:53:22 +0800

    Realign with Microsoft.IO.RecyclableMemoryStream.

--- src/CuteAnt.Runtime/IO/RecyclableMemoryStream/MemoryStreamManager.cs
+++ src/CuteAnt.Runtime/IO/RecyclableMemoryStream/MemoryStreamManager.cs
@@ -7,25 +7,29 @@ namespace CuteAnt.IO
   {
     private const string c_tag = nameof(MemoryStreamManager);
 
-    private static bool _UseRecyclableMemoryStream = true;
-    public static bool UseRecyclableMemoryStream { get => _UseRecyclableMemoryStream; set => _UseRecyclableMemoryStream = value; }
+    public static bool UseRecyclableMemoryStream { get; set; } = true;
 
-    public static readonly RecyclableMemoryStreamManager RecyclableInstance = new RecyclableMemoryStreamManager();
+    private static readonly RecyclableMemoryStreamManager RecyclableInstance;
+
+    static MemoryStreamManager()
+    {
+      RecyclableInstance = new RecyclableMemoryStreamManager();
+    }
 
     [MethodImpl(InlineMethod.Value)]
     public static MemoryStream GetStream()
-        => _UseRecyclableMemoryStream ? RecyclableInstance.GetStream(c_tag) : new MemoryStream();
+        => UseRecyclableMemoryStream ? RecyclableInstance.GetStream(c_tag) : new MemoryStream();
 
     [MethodImpl(InlineMethod.Value)]
     public static MemoryStream GetStream(int capacity)
-        => _UseRecyclableMemoryStream ? RecyclableInstance.GetStream(c_tag, capacity, true) : new MemoryStream(capacity);
+        => UseRecyclableMemoryStream ? RecyclableInstance.GetStream(c_tag, capacity, true) : new MemoryStream(capacity);
 
     [MethodImpl(InlineMethod.Value)]
     public static MemoryStream GetStream(byte[] bytes)
-        => _UseRecyclableMemoryStream ? RecyclableInstance.GetStream(c_tag, bytes, 0, bytes.Length) : new MemoryStream(bytes);
+        => UseRecyclableMemoryStream ? RecyclableInstance.GetStream(c_tag, bytes, 0, bytes.Length) : new MemoryStream(bytes);
 
     [MethodImpl(InlineMethod.Value)]
     public static MemoryStream GetStream(byte[] bytes, int index, int count)
-        => _UseRecyclableMemoryStream ? RecyclableInstance.GetStream(c_tag, bytes, index, count) : new MemoryStream(bytes, index, count);
+        => UseRecyclableMemoryStream ? RecyclableInstance.GetStream(c_tag, bytes, index, count) : new MemoryStream(bytes, index, count);
   }
 }
