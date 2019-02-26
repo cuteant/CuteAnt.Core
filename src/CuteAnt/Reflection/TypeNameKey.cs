3819e90b525171b9e3ea3b10d88ff4ae925bda2f
cuteant <cuteant@outlook.com>
2019-02-27 01:32:13 +0800

    Refactor `ResolveType`

--- src/CuteAnt/Reflection/TypeNameKey.cs
+++ src/CuteAnt/Reflection/TypeNameKey.cs
@@ -5,137 +5,133 @@ using System.Runtime.CompilerServices;
 
 namespace CuteAnt.Reflection
 {
-  // 这儿的 TypeName 存储的是 Type.AssemblyQualifiedName, 与 Newtonsoft.Json 不同需要注意
-  internal readonly struct TypeNameKey : IEquatable<TypeNameKey>
-  {
-    internal readonly string AssemblyName;
-    internal readonly string TypeName;
-
-    public TypeNameKey(string assemblyName, string typeName)
+    internal readonly struct TypeNameKey : IEquatable<TypeNameKey>
     {
-      AssemblyName = assemblyName;
-      TypeName = typeName;
+        internal readonly string AssemblyName;
+        internal readonly string TypeName;
+
+        public TypeNameKey(string assemblyName, string typeName)
+        {
+            AssemblyName = assemblyName;
+            TypeName = typeName;
+        }
+
+        public override int GetHashCode()
+        {
+            var comparer = StringComparer.Ordinal;
+            return (AssemblyName != null ? comparer.GetHashCode(AssemblyName) : 0) ^
+                   (TypeName != null ? comparer.GetHashCode(TypeName) : 0);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is TypeNameKey nameKey) { return Equals(nameKey); }
+            return false;
+        }
+
+        public bool Equals(TypeNameKey other)
+        {
+            return (string.Equals(AssemblyName, other.AssemblyName, StringComparison.Ordinal) &&
+                    string.Equals(TypeName, other.TypeName, StringComparison.Ordinal));
+        }
     }
 
-    public override int GetHashCode()
+    internal sealed class TypeNameKeyComparer : IComparer, IEqualityComparer, IComparer<TypeNameKey>, IEqualityComparer<TypeNameKey>
     {
-      //var comparer = StringComparer.Ordinal;
-      //return (AssemblyName != null ? comparer.GetHashCode(AssemblyName) : 0) ^
-      //       (TypeName != null ? comparer.GetHashCode(TypeName) : 0);
-      return StringComparer.Ordinal.GetHashCode(TypeName);
+        /// <summary>Default</summary>
+        public static readonly TypeNameKeyComparer Default;
+
+        private static readonly StringComparer s_stringComparer;
+
+        static TypeNameKeyComparer()
+        {
+            Default = new TypeNameKeyComparer();
+            s_stringComparer = StringComparer.Ordinal;
+        }
+
+        #region -- IComparer Members --
+
+        /// <inheritdoc />
+        public int Compare(object x, object y)
+        {
+            if (x == y) { return 0; }
+            if (x == null) { return -1; }
+            if (y == null) { return 1; }
+
+            if (x is TypeNameKey obj1 && y is TypeNameKey obj2)
+            {
+                return Compare(obj1, obj2);
+            }
+            return ThrowArgumentException();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static int ThrowArgumentException()
+        {
+            throw GetArgumentException();
+
+            ArgumentException GetArgumentException()
+            {
+                return new ArgumentException("类型不是 TypeNameKey");
+            }
+        }
+
+        #endregion
+
+        #region -- IComparer<CombGuid> Members --
+
+        /// <inheritdoc />
+        public int Compare(TypeNameKey x, TypeNameKey y)
+        {
+            var v = s_stringComparer.Compare(x.TypeName, y.TypeName);
+            if (v == 0)
+            {
+                return s_stringComparer.Compare(x.AssemblyName, y.AssemblyName);
+            }
+            return v;
+        }
+
+        #endregion
+
+        #region -- IEqualityComparer --
+
+        /// <inheritdoc />
+        public new bool Equals(object x, object y)
+        {
+            if (x == y) return true;
+            if (x == null || y == null) return false;
+
+            if (x is TypeNameKey key)
+            {
+                return key.Equals(y);
+            }
+            return x.Equals(y);
+        }
+
+        /// <inheritdoc />
+        public int GetHashCode(object obj)
+        {
+            if (obj == null) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.obj); }
+
+            if (obj is TypeNameKey) { return ((TypeNameKey)obj).GetHashCode(); }
+
+            return obj.GetHashCode();
+        }
+
+        #endregion
+
+        #region -- IEqualityComparer<TypeNameKey> Members --
+
+        /// <inheritdoc />
+        public bool Equals(TypeNameKey x, TypeNameKey y)
+        {
+            return s_stringComparer.Equals(x.AssemblyName, y.AssemblyName) &&
+                   s_stringComparer.Equals(x.TypeName, y.TypeName);
+        }
+
+        /// <inheritdoc />
+        public int GetHashCode(TypeNameKey obj) => obj.GetHashCode();
+
+        #endregion
     }
-
-    public override bool Equals(object obj)
-    {
-      if (obj is TypeNameKey nameKey) { return Equals(nameKey); }
-      return false;
-    }
-
-    public bool Equals(TypeNameKey other)
-    {
-      return StringComparer.Ordinal.Equals(TypeName, other.TypeName);
-      //return (string.Equals(AssemblyName, other.AssemblyName, StringComparison.Ordinal) &&
-      //        string.Equals(TypeName, other.TypeName, StringComparison.Ordinal));
-    }
-  }
-
-  internal class TypeNameKeyComparer : IComparer, IEqualityComparer, IComparer<TypeNameKey>, IEqualityComparer<TypeNameKey>
-  {
-    /// <summary>Default</summary>
-    public static readonly TypeNameKeyComparer Default;
-
-    private static readonly StringComparer s_stringComparer;
-
-    static TypeNameKeyComparer()
-    {
-      Default = new TypeNameKeyComparer();
-      s_stringComparer = StringComparer.Ordinal;
-    }
-
-    #region -- IComparer Members --
-
-    /// <inheritdoc />
-    public int Compare(object x, object y)
-    {
-      if (x == y) { return 0; }
-      if (x == null) { return -1; }
-      if (y == null) { return 1; }
-
-      if (x is TypeNameKey obj1 && y is TypeNameKey obj2)
-      {
-        return Compare(obj1, obj2);
-      }
-      return ThrowArgumentException();
-    }
-
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    private static int ThrowArgumentException()
-    {
-      throw GetArgumentException();
-
-      ArgumentException GetArgumentException()
-      {
-        return new ArgumentException("类型不是 TypeNameKey");
-      }
-    }
-
-    #endregion
-
-    #region -- IComparer<CombGuid> Members --
-
-    /// <inheritdoc />
-    public int Compare(TypeNameKey x, TypeNameKey y)
-    {
-      var v = s_stringComparer.Compare(x.TypeName, y.TypeName);
-      //if (v == 0)
-      //{
-      //  return s_stringComparer.Compare(x.AssemblyName, y.AssemblyName);
-      //}
-      return v;
-    }
-
-    #endregion
-
-    #region -- IEqualityComparer --
-
-    /// <inheritdoc />
-    public new bool Equals(object x, object y)
-    {
-      if (x == y) return true;
-      if (x == null || y == null) return false;
-
-      if (x is TypeNameKey key)
-      {
-        return key.Equals(y);
-      }
-      return x.Equals(y);
-    }
-
-    /// <inheritdoc />
-    public int GetHashCode(object obj)
-    {
-      if (obj == null) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.obj); }
-
-      if (obj is TypeNameKey) { return ((TypeNameKey)obj).GetHashCode(); }
-
-      return obj.GetHashCode();
-    }
-
-    #endregion
-
-    #region -- IEqualityComparer<TypeNameKey> Members --
-
-    /// <inheritdoc />
-    public bool Equals(TypeNameKey x, TypeNameKey y)
-    {
-      return s_stringComparer.Equals(x.TypeName, y.TypeName);
-      //return (string.Equals(x.AssemblyName, y.AssemblyName, StringComparison.Ordinal) &&
-      //        string.Equals(x.TypeName, y.TypeName, StringComparison.Ordinal));
-    }
-
-    /// <inheritdoc />
-    public int GetHashCode(TypeNameKey obj) => s_stringComparer.GetHashCode(obj.TypeName);// => obj.GetHashCode();
-
-    #endregion
-  }
 }
--- src/CuteAnt/Serialization/DefaultSerializationBinder.cs
+++ src/CuteAnt/Serialization/DefaultSerializationBinder.cs
@@ -5,35 +5,32 @@ using CuteAnt.Reflection;
 
 namespace CuteAnt.Serialization
 {
-  /// <summary>The default serialization binder used when resolving and loading classes from type names.</summary>
-  public class DefaultSerializationBinder : SerializationBinder
-  {
-    /// <summary>Initializes a new instance of the <see cref="DefaultSerializationBinder"/> class.</summary>
-    public DefaultSerializationBinder()
+    /// <summary>The default serialization binder used when resolving and loading classes from type names.</summary>
+    public class DefaultSerializationBinder : SerializationBinder
     {
-    }
+        /// <summary>Initializes a new instance of the <see cref="DefaultSerializationBinder"/> class.</summary>
+        public DefaultSerializationBinder()
+        {
+        }
 
-    /// <summary>When overridden in a derived class, controls the binding of a serialized object to a type.</summary>
-    /// <param name="assemblyName">Specifies the <see cref="Assembly"/> name of the serialized object.</param>
-    /// <param name="typeName">Specifies the <see cref="System.Type"/> name of the serialized object.</param>
-    /// <returns>The type of the object the formatter creates a new instance of.</returns>
-    public override Type BindToType(string assemblyName, string typeName)
-    {
-      //return TypeUtils.ResolveType(new TypeNameKey(assemblyName, typeName));
+        /// <summary>When overridden in a derived class, controls the binding of a serialized object to a type.</summary>
+        /// <param name="assemblyName">Specifies the <see cref="Assembly"/> name of the serialized object.</param>
+        /// <param name="typeName">Specifies the <see cref="System.Type"/> name of the serialized object.</param>
+        /// <returns>The type of the object the formatter creates a new instance of.</returns>
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            return TypeUtils.ResolveType(new TypeNameKey(assemblyName, typeName));
+        }
 
-      // 这儿的 TypeName 存储的是 Type.AssemblyQualifiedName, 与 Newtonsoft.Json 不同需要注意
-      return TypeUtils.ResolveType(new TypeNameKey(assemblyName, typeName + (assemblyName == null ? "" : ", " + assemblyName)));
+        /// <summary>When overridden in a derived class, controls the binding of a serialized object to a type.</summary>
+        /// <param name="serializedType">The type of the object the formatter creates a new instance of.</param>
+        /// <param name="assemblyName">Specifies the <see cref="Assembly"/> name of the serialized object.</param>
+        /// <param name="typeName">Specifies the <see cref="System.Type"/> name of the serialized object.</param>
+        public override void BindToName(Type serializedType, out string assemblyName, out string typeName)
+        {
+            var typeDef = RuntimeTypeNameFormatter.GetTypeDefinition(serializedType);
+            assemblyName = typeDef.AssemblyName;
+            typeName = typeDef.TypeName;
+        }
     }
-
-    /// <summary>When overridden in a derived class, controls the binding of a serialized object to a type.</summary>
-    /// <param name="serializedType">The type of the object the formatter creates a new instance of.</param>
-    /// <param name="assemblyName">Specifies the <see cref="Assembly"/> name of the serialized object.</param>
-    /// <param name="typeName">Specifies the <see cref="System.Type"/> name of the serialized object.</param>
-    public override void BindToName(Type serializedType, out string assemblyName, out string typeName)
-    {
-      var typeDef = RuntimeTypeNameFormatter.GetTypeDefinition(serializedType);
-      assemblyName = typeDef.AssemblyName;
-      typeName = typeDef.TypeName;
-    }
-  }
 }
